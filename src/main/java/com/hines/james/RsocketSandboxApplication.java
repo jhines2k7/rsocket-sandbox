927f3b443356c31788a47b34695e4e93be365584
James Hines <jameshines10@gmail.com>
2020-06-06 19:29:44 -0500

    Creates a CommandLineRunner bean

--- src/main/java/com/hines/james/RsocketSandboxApplication.java
+++ src/main/java/com/hines/james/RsocketSandboxApplication.java
@@ -1,7 +1,9 @@
 package com.hines.james;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RsocketSandboxApplication {
@@ -10,4 +12,10 @@ public class RsocketSandboxApplication {
 		SpringApplication.run(RsocketSandboxApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
