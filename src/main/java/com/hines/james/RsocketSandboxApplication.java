41b53cd05fd4ff2fc0d5890b36fb0ae20f6dab29
James Hines <jameshines10@gmail.com>
2020-06-06 19:26:45 -0500

    Initial commit

---
+++ src/main/java/com/hines/james/RsocketSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketSandboxApplication.class, args);
+	}
+
+}
